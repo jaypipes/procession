fa5ad324cbb7bc616a3a9e7b14ab65652672a8b3
Jay Pipes <jaypipes@gmail.com>
2017-05-07 22:54:24 -0400

    Add ordered UUID1 utility package and function

---
+++ pkg/util/uuid.go
@@ -0,0 +1,48 @@
+package util
+
+import (
+    "encoding/hex"
+
+    "github.com/pborman/uuid"
+)
+
+// Returns a new "ordered" UUID as 32 alphanumeric characters with no dashes --
+// the most efficient string representation for storage in a DB (besides
+// storing as BINARY, which makes querying ugly and overly difficult for little
+// benefit)
+// 
+// Returns a UUID type 1 value, with the more constant segments of the
+// UUID at the start of the UUID. This allows us to have mostly monotonically
+// increasing UUID values, which are much better for INSERT/UPDATE performance
+// in the DB.
+//
+// A UUID1 hex looks like:
+//
+// '27392da28bae11e4961de06995034837'
+//
+// From this, we need to take the last two segments, which represent the more
+// constant information about the node we're on, and place those first in the
+// new UUID's bytes. We then take the '11e4' segment, which represents the
+// most significant bits of the timestamp part of the UUID, prefixed with a
+// '1' for UUID type, and place that next, followed by the second segment and
+// finally the first segment, which are the next most significant bits of the
+// timestamp 60-bit number embedded in the UUID.
+//
+// So, we convert the above hex to this instead:
+//
+// '961de0699503483711e48bae27392da2'
+//
+func OrderedUuid() (string) {
+    u := uuid.NewUUID()
+
+    var o [16]byte
+    var y [32]byte
+
+    copy(o[0:], u[8:])
+    copy(o[8:], u[6:8])
+    copy(o[10:], u[4:6])
+    copy(o[12:], u[0:4])
+
+    hex.Encode(y[:], o[:])
+    return string(y[:])
+}
