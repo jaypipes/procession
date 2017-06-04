c1305405d94c662e5ed3736f4d3c7b96dc89a9aa
Jay Pipes <jaypipes@gmail.com>
2017-06-04 14:31:35 -0400

    Forgot to actually add the p7n/commands/user_members.go file

---
+++ p7n/commands/user_members.go
@@ -0,0 +1,92 @@
+package commands
+
+import (
+    "fmt"
+    "io"
+    "os"
+    "golang.org/x/net/context"
+
+    "github.com/spf13/cobra"
+    "github.com/olekukonko/tablewriter"
+    pb "github.com/jaypipes/procession/proto"
+)
+
+var (
+    userMembersUserId string
+)
+
+var userMembersCommand = &cobra.Command{
+    Use: "members <organization> [add|remove <users> ...]",
+    Short: "List and change members of an organization",
+    RunE: userMembers,
+}
+
+func userMembers(cmd *cobra.Command, args []string) error {
+    checkAuthUser(cmd)
+    if len(args) != 1 {
+        fmt.Println("Please specify a user identifier.")
+        cmd.Usage()
+        return nil
+    }
+
+    userMembersUserId = args[0]
+    return userMembersList(cmd, userMembersUserId)
+}
+
+func userMembersList(cmd *cobra.Command, userId string) error {
+    checkAuthUser(cmd)
+    conn, err := connect()
+    if err != nil {
+        return err
+    }
+    defer conn.Close()
+
+    client := pb.NewIAMClient(conn)
+    req := &pb.UserMembersListRequest{
+        Session: &pb.Session{User: authUser},
+        User: userId,
+    }
+    stream, err := client.UserMembersList(
+        context.Background(),
+        req,
+    )
+    if err != nil {
+        return err
+    }
+
+    orgs := make([]*pb.Organization, 0)
+    for {
+        org, err := stream.Recv()
+        if err == io.EOF {
+            break
+        }
+        if err != nil {
+            return err
+        }
+        orgs = append(orgs, org)
+    }
+    headers := []string{
+        "UUID",
+        "Display Name",
+        "Slug",
+        "Parent",
+    }
+    rows := make([][]string, len(orgs))
+    for x, org := range orgs {
+        parentUuid := ""
+        if org.ParentUuid != nil {
+            parentUuid = org.ParentUuid.Value
+        }
+        rows[x] = []string{
+            org.Uuid,
+            org.DisplayName,
+            org.Slug,
+            parentUuid,
+        }
+    }
+    table := tablewriter.NewWriter(os.Stdout)
+    table.SetHeader(headers)
+    table.AppendBulk(rows)
+    table.Render()
+    return nil
+}
