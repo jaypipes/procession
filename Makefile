2c239233b0682692a0f49085e757aa2000f13241
Jay Pipes <jaypipes@gmail.com>
2017-06-04 16:53:10 -0400

    start build and test automation
    Adds rudimentary Makefile that builds the two current Procession components
    (IAM server and p7n CLI tool).
    
    Also adds a Makefile target for a simple end-to-end testing framework. The
    first simple test in the framework is a silly ping of dependent endpoints.
    
    Issue #26
    Issue #27

--- Makefile
+++ Makefile
@@ -7,3 +7,19 @@ generated:
 	protoc -I $(PROTO_DEFS_DIR) \
 	       $(PROTO_DEFS_DIR)/*.proto \
 	       --go_out=plugins=grpc:$(PROTO_DIR)
+
+build-iam: generated
+	@echo "Building procession-iamd ..."
+	go build -o build/bin/procession-iamd iam/main.go
+
+build-p7n: generated
+	@echo "Building p7n client tool ..."
+	go build -o build/bin/p7n p7n/main.go
+
+e2e: build-iam build-p7n
+	@echo "Running end-to-end tests ..."
+	./e2e/run_tests.bash
+
+build: build-iam build-p7n
+
+test: e2e
---
+++ e2e/run_tests.bash
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+set -o nounset
+
+ROOTDIR="$(readlink -f $(dirname $(dirname $0)))"
+E2EDIR="$ROOTDIR/e2e"
+ERRLOG=$E2EDIR/err.log
+
+truncate -s 0 $ERRLOG
+
+if [ -f $ROOTDIR/.processionrc ] ; then
+    source $ROOTDIR/.processionrc
+fi
+
+num_tests_run=0
+num_tests_passed=0
+num_tests_skipped=0
+num_tests_failed=0
+total_time_elapsed="0 seconds"
+
+echo "Running end to end functional integration tests for Procession..."
+echo ""
+echo "Configuration:"
+echo "------------------------------------------------------------------------"
+echo "Root dir:              $ROOTDIR"
+echo "Run on:                $(date -u)"
+echo "Git SHA:               $(git describe --tags --always --dirty)"
+echo "PROCESSION_USER:       ${PROCESSION_USER:-}"
+echo "PROCESSION_LOG_LEVEL:  ${PROCESSION_LOG_LEVEL:-}"
+echo "GSR_ETCD_ENDPOINTS:    ${GSR_ETCD_ENDPOINTS:-}"
+echo "GSR_LOG_LEVEL:         ${GSR_LOG_LEVEL:-}"
+echo "------------------------------------------------------------------------"
+echo "Starting tests ..."
+
+
+for t in ping_endpoints; do
+    printf "    %-63s "  "$t ..."
+    . $E2EDIR/tests/$t
+    rc=$?
+    if [[ $rc -eq 0 ]]; then
+        echo "  ok"
+        num_tests_passed=$(( $num_tests_passed + 1 ))
+    elif [[ $rc -eq 1 ]]; then
+        echo "fail"
+        num_tests_failed=$(( $num_tests_failed + 1 ))
+    else
+        echo "skip"
+        num_tests_skipped=$(( $num_tests_skipped + 1 ))
+    fi
+done
+
+echo "------------------------------------------------------------------------"
+echo "Completed $num_tests_run tests in $total_time_elapsed"
+echo "Passed:   $num_tests_passed"
+echo "Skipped:  $num_tests_skipped"
+echo "Failed:   $num_tests_failed"
+
+if [[ $num_tests_failed -gt 0 ]]; then
+    echo "Errors in $ERRLOG:"
+    cat $ERRLOG
+fi
+
+exit $num_tests_failed
