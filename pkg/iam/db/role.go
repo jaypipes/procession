f073fea13d18409bd663e0d5e60e9705f6544572
Jay Pipes <jaypipes@gmail.com>
2017-06-28 12:40:57 -0400

    authz: add role set DB/RPC plumbing
    Adds code to create or update a role to the IAM service.
    
    Issue #11

--- pkg/iam/db/role.go
+++ pkg/iam/db/role.go
@@ -2,7 +2,12 @@ package db
 
 import (
     "database/sql"
+    "fmt"
     "log"
+    "strings"
+
+    "github.com/gosimple/slug"
+    "github.com/go-sql-driver/mysql"
 
     "github.com/jaypipes/procession/pkg/context"
     "github.com/jaypipes/procession/pkg/util"
@@ -120,4 +125,156 @@ WHERE rp.role_id = ?
         perms = append(perms, pb.Permission(perm))
     }
     return perms, nil
+}
+
+// Creates a new record for an role
+func RoleCreate(
+    sess *pb.Session,
+    ctx *context.Context,
+    fields *pb.RoleSetFields,
+) (*pb.Role, error) {
+    reset := ctx.LogSection("iam/db")
+    defer reset()
+    db := ctx.Db
+    tx, err := db.Begin()
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer tx.Rollback()
+
+    uuid := util.Uuid4Char32()
+    displayName := fields.DisplayName.Value
+    slug := slug.Make(displayName)
+    var rootOrgId interface{}
+    if fields.OrganizationUuid != nil {
+        rootOrgUuid := fields.OrganizationUuid.Value
+        rootOrgInternalId := orgIdFromIdentifier(ctx, rootOrgUuid)
+        if rootOrgInternalId == 0 {
+            err = fmt.Errorf("No such organization %s", rootOrgUuid)
+            return nil, err
+        }
+        rootOrgId = rootOrgInternalId
+    }
+
+    qs := `
+INSERT INTO roles (
+  uuid
+, display_name
+, slug
+, root_organization_id
+, generation
+) VALUES (?, ?, ?, ?, ?)
+`
+
+    ctx.LSQL(qs)
+
+    stmt, err := tx.Prepare(qs)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer stmt.Close()
+    _, err = stmt.Exec(
+        uuid,
+        displayName,
+        slug,
+        rootOrgId,
+        1,
+    )
+    if err != nil {
+        me, ok := err.(*mysql.MySQLError)
+        if !ok {
+            return nil, err
+        }
+        if me.Number == 1062 {
+            // Duplicate key, check if it's the slug...
+            if strings.Contains(me.Error(), "uix_slug_root_organization_id") {
+                return nil, fmt.Errorf("Duplicate display name.")
+            }
+        }
+        return nil, err
+    }
+
+    err = tx.Commit()
+    if err != nil {
+        return nil, err
+    }
+
+    role := &pb.Role{
+        Uuid: uuid,
+        DisplayName: displayName,
+        Slug: slug,
+        OrganizationUuid: fields.OrganizationUuid,
+        Generation: 1,
+    }
+
+    ctx.L2("Created new root role %s (%s)", slug, uuid)
+    return role, nil
+}
+
+// Updates information for an existing role by examining the fields
+// changed to the current fields values
+func RoleUpdate(
+    ctx *context.Context,
+    before *pb.Role,
+    changed *pb.RoleSetFields,
+) (*pb.Role, error) {
+    reset := ctx.LogSection("iam/db")
+    defer reset()
+    db := ctx.Db
+    uuid := before.Uuid
+    qs := "UPDATE roles SET "
+    changes := make(map[string]interface{}, 0)
+    newRole := &pb.Role{
+        Uuid: uuid,
+        Generation: before.Generation + 1,
+    }
+    if changed.DisplayName != nil {
+        newDisplayName := changed.DisplayName.Value
+        newSlug := slug.Make(newDisplayName)
+        changes["display_name"] = newDisplayName
+        changes["slug"] = newSlug
+        newRole.DisplayName = newDisplayName
+        newRole.Slug = newSlug
+    } else {
+        newRole.DisplayName = before.DisplayName
+        newRole.Slug = before.Slug
+    }
+    for field, _ := range changes {
+        qs = qs + fmt.Sprintf("%s = ?, ", field)
+    }
+    // Trim off the last comma and space
+    qs = qs[0:len(qs)-2]
+
+    qs = qs + "\nWHERE uuid = ? AND generation = ?"
+
+    ctx.LSQL(qs)
+
+    stmt, err := db.Prepare(qs)
+    if err != nil {
+        return nil, err
+    }
+    pargs := make([]interface{}, len(changes) + 2)
+    x := 0
+    for _, value := range changes {
+        pargs[x] = value
+        x++
+    }
+    pargs[x] = uuid
+    x++
+    pargs[x] = before.Generation
+    _, err = stmt.Exec(pargs...)
+    if err != nil {
+        me, ok := err.(*mysql.MySQLError)
+        if !ok {
+            return nil, err
+        }
+        if me.Number == 1062 {
+            // Duplicate key, check if it's the slug...
+            if strings.Contains(me.Error(), "uix_slug_root_organization_id") {
+                return nil, fmt.Errorf("Duplicate display name.")
+            }
+        }
+        return nil, err
+    }
+    return newRole, nil
 }
--- pkg/iam/server/role.go
+++ pkg/iam/server/role.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+    "fmt"
+
     "golang.org/x/net/context"
 
     pb "github.com/jaypipes/procession/proto"
@@ -24,4 +26,54 @@ func (s *Server) RoleGet(
         return nil, err
     }
     return role, nil
+}
+
+// RoleSet creates a new role or updates an existing
+// role
+func (s *Server) RoleSet(
+    ctx context.Context,
+    req *pb.RoleSetRequest,
+) (*pb.RoleSetResponse, error) {
+    reset := s.Ctx.LogSection("iam/server")
+    defer reset()
+    changed := req.Changed
+
+    if req.Search == nil {
+        s.Ctx.L3("Creating new role")
+
+        newRole, err := db.RoleCreate(
+            req.Session,
+            s.Ctx,
+            changed,
+        )
+        if err != nil {
+            return nil, err
+        }
+        resp := &pb.RoleSetResponse{
+            Role: newRole,
+        }
+        s.Ctx.L1("Created new role %s", newRole.Uuid)
+        return resp, nil
+    }
+
+    s.Ctx.L3("Updating role %s", req.Search.Value)
+
+    before, err := db.RoleGet(s.Ctx, req.Search.Value)
+    if err != nil {
+        return nil, err
+    }
+    if before.Uuid == "" {
+        notFound := fmt.Errorf("No such role found.")
+        return nil, notFound
+    }
+
+    newRole, err := db.RoleUpdate(s.Ctx, before, changed)
+    if err != nil {
+        return nil, err
+    }
+    resp := &pb.RoleSetResponse{
+        Role: newRole,
+    }
+    s.Ctx.L1("Updated role %s", newRole.Uuid)
+    return resp, nil
 }
